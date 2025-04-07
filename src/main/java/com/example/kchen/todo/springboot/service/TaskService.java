6a22f51defaae44fcd3c8fc21af33cd587790863
KChen243 <kalvin.chen@bertelsmann.de>
2025-04-07 10:09:34 +0800

    added task service

---
+++ src/main/java/com/example/kchen/todo/springboot/service/TaskService.java
@@ -0,0 +1,42 @@
+package com.example.kchen.todo.springboot.service;
+
+import com.example.kchen.todo.springboot.entity.Task;
+import com.example.kchen.todo.springboot.repository.TaskDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+	private TaskDao taskDao;
+
+	@Autowired
+	public TaskService(TaskDao taskDao) {
+		this.taskDao = taskDao;
+	}
+
+	public List<Task> findAll() {
+		return this.taskDao.findAll();
+	}
+
+	public Task findById(int id) {
+		return this.taskDao.findById(id);
+	}
+
+	@Transactional
+	public Task add(Task newTask) {
+		return this.taskDao.add(newTask);
+	}
+
+	@Transactional
+	public Task update(Task udpatedTask) {
+		return this.taskDao.add(udpatedTask);
+	}
+
+	@Transactional
+	public Task deleteById(int id) {
+		return this.taskDao.deleteById(id);
+	}
+}
