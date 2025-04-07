e26149d27949c3e010ea97e692a6eded548772d4
KChen243 <kalvin.chen@bertelsmann.de>
2025-04-07 10:35:39 +0800

    added constructor

--- src/main/java/com/example/kchen/todo/springboot/entity/Task.java
+++ src/main/java/com/example/kchen/todo/springboot/entity/Task.java
@@ -1,15 +1,20 @@
 package com.example.kchen.todo.springboot.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 import java.util.Date;
 
-@Entity(name = "task")
+@Entity
+@Table(name = "task")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
