44a7a5d4108937bdc18d48aa352ee84f811c4e7f
KChen243 <kalvin.chen@bertelsmann.de>
2025-04-14 09:21:19 +0800

    used lombok for getter and setter

--- src/main/java/com/example/kchen/todo/springboot/jwt/CustomJwt.java
+++ src/main/java/com/example/kchen/todo/springboot/jwt/CustomJwt.java
@@ -1,11 +1,15 @@
 package com.example.kchen.todo.springboot.jwt;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Collection;
 
+@Getter
+@Setter
 public class CustomJwt extends JwtAuthenticationToken {
 
 	private String firstname;
@@ -14,21 +18,5 @@ public class CustomJwt extends JwtAuthenticationToken {
 
 	public CustomJwt(Jwt jwt, Collection<? extends GrantedAuthority> authorities) {
 		super(jwt, authorities);
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
 	}
 }
